8bbda43761660d7e3d79e45fefa02da5d1c3c369
ChouTzuyu <tew_ki24@hotmail.com>
2022-08-09 09:56:14 +0700

    อัพครั้งที่ 2
    เพิ่มหน้า categories  และเชื่อมหน้า

--- resources/views/includes/promote/header.blade.php
+++ resources/views/includes/promote/header.blade.php
@@ -5,19 +5,19 @@
                 <div class="col-lg-12">
                     <div class="header__nav">
                     <div class="header__logo">
-                        <a href="./index.php">
+                        <a href="{{ url('/') }}">
                             <img src="{{ asset('template/promote/img/headerlogo.png') }}" alt="">
                         </a>
                     <div class="header__right">
                         <nav class="header__menu mobile-menu">
                             <ul>
-                                <li class="active"><a href="./index.php">Homepage</a></li>
-                                <li><a href="./categories.php">Categories</a></li>
-                                <li><a href="./content.php">Content</a></li>
-                                <li><a href="./blog.php">Our Blog</a></li>
-                                <li><a href="#">Contacts</a></li>
-                                <li><a href="./login.php">Login</a></li>
-                                <li><a href="./signup.php">Sign Up</a></li>
+                                <li class="active"><a href="{{ url('/') }}">Homepage</a></li>
+                                <li><a href="{{ url('/categories') }}">Categories</a></li>
+                                <li><a href="{{ url('/') }}">Content</a></li>
+                                <li><a href="{{ url('/') }}">Our Blog</a></li>
+                                <li><a href="{{ url('/') }}">Contacts</a></li>
+                                <li><a href="{{ url('/') }}">Login</a></li>
+                                <li><a href="{{ url('/') }}">Sign Up</a></li>
                                 <a href="#" class="search-switch"><span class="icon_search"></span></a>
                                 <a href="./login.php"><span class="icon_profile"></span></a>
                     </div>
